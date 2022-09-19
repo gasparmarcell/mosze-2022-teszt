bf3b82a211bc64c3a43c4ba2d4d2b11af5fc4f20
gasparmarcell <gaspar.marcell97@gmail.com>
2022-09-19 18:48:22 +0200

    hibak kommentelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // rosszul van leírva az N_ELEMENTS  
+    std::cout << '1-100 ertekek duplazasa' // aposztróf helyett idézőjel + pontosvessző
+    for (int i = 0;) // helytelen for ciklus + felesleges for ciklus egybe lehet építeni
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) 
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //az "Ertek" után nem ír ki számokat
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // atlag kezdőérték hiányzik + átlag számításnál elönyösebb double-t használni
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // pontosvessző hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
